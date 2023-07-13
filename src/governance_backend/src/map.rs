e2cb48610bca7d6ce85b23144a0de8c80ca80b10
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-13 19:26:15 +0800

    Added stable memory collection.

---
+++ src/governance_backend/src/map.rs
@@ -0,0 +1,20 @@
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
+use std::cell::RefCell;
+use crate::law::Law;
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+thread_local! {
+    // The memory manager is used for simulating multiple memories. Given a `MemoryId` it can
+    // return a memory that can be used by stable structures.
+    pub (crate) static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    // Initialize a `StableBTreeMap` with `MemoryId(0)`.
+    pub  (crate) static MAP: RefCell<StableBTreeMap<u128, Law, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
+        )
+    );
+}
