63375f73caf90676f42c03cb9b8b9cb2fb04a32d
Marvin Hansen <marvin.hansen@gmail.com>
2023-07-13 19:26:43 +0800

    Implemented interface

--- src/governance_backend/src/lib.rs
+++ src/governance_backend/src/lib.rs
@@ -1,4 +1,18 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use crate::law::Law;
+use crate::map::MAP;
+
+mod map;
+mod law;
+
+
+#[ic_cdk_macros::query]
+fn get_law(key: u128) -> Option<Law>
+{
+    MAP.with(|p| p.borrow().get(&key))
+}
+
+#[ic_cdk_macros::update]
+fn set_law(key: u128, value: Law) -> Option<Law>
+{
+    MAP.with(|p| p.borrow_mut().insert(key, value))
 }
